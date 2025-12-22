0035dd1e3cc2ec4388ba696ba9e03aff025d494d
ingyu <ingyu@bag-ingyuui-MacBookPro.local>
2025-12-22 11:35:31 +0900

    ✨ Feat: 충전하기 모달 내용 수정 및 후원 모달 기능 구현
    충전하기 모달 로컬스토리지 연동

--- Pandom-k/src/components/CreditChargeModal.jsx
+++ Pandom-k/src/components/CreditChargeModal.jsx
@@ -1,39 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import BaseModal from './BaseModal';
 import { ButtonContent, ChargeButton, ChargeInput, Credit, RadioCircle, SelectButton, SuccessIcon } from './CreditChargeModal.styled.js';
 import creditImg from '../assets/icons/credit.svg'
 
-function CreditChargeModal({ isOpen, onClose }) {
+function CreditChargeModal({ isOpen, onClose, myCredit, setMyCredit }) {
   const [inputAmount, setInputAmount] = useState('');
   const [selectedAmount, setSelectedAmount] = useState(null);
   const finalChargeAmount = selectedAmount ?? Number(inputAmount);
   const isDisabled = !finalChargeAmount || finalChargeAmount <= 0;
-  const getStorageCredit = () => {
-    const storage = localStorage.getItem('credit');
-    return storage ? Number(storage) : 0;
-  }
   const [modalStep, setModalStep] = useState('charge')
-  const handleCharge = () => {
-    const currentCredit = getStorageCredit();
-    const newCredit = currentCredit + finalChargeAmount;
 
-    localStorage.setItem('credit', String(newCredit));
+  const handleCharge = () => {
+    const amountToCharge = selectedAmount ?? Number(inputAmount);
+    setMyCredit((prev) => prev + amountToCharge);
 
     setModalStep('success');
   };
+
   const handleClose = () => {
     setModalStep('charge');
     setSelectedAmount(null);
     setInputAmount('');
     onClose();
   }
+
+  useEffect(() => {
+  if (!isOpen) {
+    setInputAmount("");
+    setSelectedAmount("");
+    setModalStep('charge');
+  }
+}, [isOpen]);
   
 
   return (
     <BaseModal isOpen={isOpen} onClose={onClose}>
       {modalStep === 'charge' && (
         <>
-
             <h2>크레딧 충전하기</h2>
             {[1000, 5000, 10000].map((amount) => (
             <SelectButton 
@@ -45,7 +48,7 @@ function CreditChargeModal({ isOpen, onClose }) {
             }}
             >
             <ButtonContent>
-            <Credit src={creditImg} alt="credit icon"/>    
+            <Credit src={creditImg} alt="credit icon"/>
             {amount}
             </ButtonContent>
             <RadioCircle $active={selectedAmount === amount} />
@@ -83,7 +86,7 @@ function CreditChargeModal({ isOpen, onClose }) {
       {modalStep === 'success' && (
         <>
           <SuccessIcon src={creditImg} alt="credit icon"/>
-          <p>충전이 완료되었습니다.</p>
+          <p>{Number(finalChargeAmount).toLocaleString()} 크레딧 충전이 완료되었습니다.</p>
           <ChargeButton onClick={handleClose}>
             확인
           </ChargeButton>
